c7d632b519e51110893f8b45cf7cf29d7c4dc183
fifaoctavia <115772098+fifaoctavia@users.noreply.github.com>
2022-12-04 15:46:38 +0700

    Add files via upload

---
+++ MODUL1/src/User.java
@@ -0,0 +1,11 @@
+public class User {
+    String Name;
+    String NIM;
+
+    public void setName(String Name){
+        this.Name = Name;
+    }
+    public void setCategory(String NIM){
+        this.NIM = NIM;
+    }
+}
